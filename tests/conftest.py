e282f14194817d3ada30d8d4ef1d2e621b3bdba2
DataWorshipper <mandalabhiraj04@gmail.com>
2026-02-21 15:13:20 +0530

    test_posts.py code added

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,9 @@
 import pytest
 from fastapi.testclient import TestClient
 from app.main import app
-from app  import schemas
+from app  import schemas,model
 from app.database import get_db,Base
+from app.oauth2 import create_access_token
 from app import model
 import os
 from urllib.parse import quote_plus
@@ -45,7 +46,14 @@ def client(session):
             session.close()
     app.dependency_overrides[get_db]=override_get_db
     yield TestClient(app)
-
+@pytest.fixture
+def test_user2(client):
+    user_data={"email":"hello12@gmail.com","password":"password123"}
+    res=client.post("/users/",json=user_data)
+    assert res.status_code==201
+    new_user=res.json()
+    new_user['password']=user_data['password']
+    return  new_user
 
 @pytest.fixture
 def test_user(client):
@@ -55,3 +63,50 @@ def test_user(client):
     new_user=res.json()
     new_user['password']=user_data['password']
     return  new_user
+
+@pytest.fixture
+def token(test_user):
+    return create_access_token({"user_id":test_user['id']})
+@pytest.fixture
+def authorized_client(client,token):
+    client.headers={
+        **client.headers,"Authorization":f"Bearer {token}"
+    }
+    return client
+    
+@pytest.fixture
+def test_posts(test_user,session,test_user2):
+    posts_data = [{
+        "title": "first title",
+        "content": "first content",
+        "owner_id": test_user['id']
+    }, {
+        "title": "2nd title",
+        "content": "2nd content",
+        "owner_id": test_user['id']
+    },
+        {
+        "title": "3rd title",
+        "content": "3rd content",
+        "owner_id": test_user['id']
+    }, {
+        "title": "3rd title",
+        "content": "3rd content",
+        "owner_id": test_user['id']
+    },
+    {
+        "title": "4th title",
+        "content": "4rd content",
+        "owner_id": test_user2['id']
+    }
+    ]
+
+    def create_post_model(post):
+        return model.Post(**post)
+
+    post_map = map(create_post_model, posts_data)
+    posts = list(post_map)
+    session.add_all(posts)
+    session.commit()
+    posts = session.query(model.Post).all()
+    return posts
