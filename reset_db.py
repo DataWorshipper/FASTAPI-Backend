2936696561f2456fdafa10bb4e93f7d78e904ff0
DataWorshipper <mandalabhiraj04@gmail.com>
2026-04-04 00:17:30 +0530

    truncate db before each test

---
+++ reset_db.py
@@ -0,0 +1,29 @@
+import os
+from sqlalchemy import create_engine, text
+# Importing the URL directly from your existing database file
+from app.database import SQLALCHEMY_DATABASE_URL 
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+def reset_database():
+  
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+    
+  
+    host = os.getenv("DB_HOST", "localhost")
+    
+    query = text("TRUNCATE posts, votes RESTART IDENTITY CASCADE;")
+    
+    try:
+        with engine.connect() as conn:
+            print(f"Connecting to host: {host}...")
+            conn.execute(query)
+            conn.commit()
+            print("Successfully cleared posts and votes. Counters reset to 1.")
+    except Exception as e:
+        print(f"Error resetting database: {e}")
+
+if __name__ == "__main__":
+    reset_database()
